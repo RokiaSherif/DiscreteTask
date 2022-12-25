9dcfbb5a7a4a7e90064d668441688bd08b04114a
RokiaSherif <121404425+RokiaSherif@users.noreply.github.com>
2022-12-25 22:56:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_numbers
+{ 
+    class perfectNumber
+    {
+      int N1,N2;
+        public perfectNumber(int N1, int N2)
+        {
+          this .  N1 = N1;
+          this.  N2 = N2;
+        }
+        public void printNumber()
+        {
+            int i,sum;
+            for (int n = N1;n <= N2; n++)
+            {
+                i =1;
+                sum =0;
+                while(i < n)
+                {
+                    if (n%i ==0)
+                    {
+                        sum =sum+i;
+                    }
+                    i++;
+                }
+                if (sum == n)
+                {
+                    Console.WriteLine("{0}",n);
+                }
+            }
+        }
+     }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please entre The first Number");
+            int N1= int.Parse(Console.ReadLine());
+            Console.WriteLine("please entre The secound Number");
+            int N2= int.Parse(Console.ReadLine());
+            perfectNumber p= new perfectNumber(N1,N2);
+            p.printNumber();
+
+        }
+        
+    }
+}
